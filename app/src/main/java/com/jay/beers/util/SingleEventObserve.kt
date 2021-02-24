249161fd33d7836af817152c820f159e7e7c315e
Jihoi-Kang <jihoi.kang@gmail.com>
2021-02-24 16:19:12 +0900

    Add single event observe

---
+++ app/src/main/java/com/jay/beers/util/SingleEventObserve.kt
@@ -0,0 +1,20 @@
+package com.jay.beers.util
+
+import androidx.annotation.MainThread
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+
+@MainThread
+inline fun <T> LiveData<Event<T>>.eventObserve(
+    owner: LifecycleOwner,
+    crossinline onChanged: (T) -> Unit,
+): Observer<Event<T>> {
+    val wrappedObserver = Observer<Event<T>> { event ->
+        event.getContentIfNotHandled()?.let {
+            onChanged.invoke(it)
+        }
+    }
+    observe(owner, wrappedObserver)
+    return wrappedObserver
+}
